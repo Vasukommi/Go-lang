af6587537447da844482eefd2cfc5c69557940c5
vasu kommi <itsvasukommi@gmail.com>
2023-01-25 22:52:22 +0530

    initial commit

---
+++ index.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare and initialize a variable
+	var x = 5
+	y := 10
+
+	// control structures
+	if x > y {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+}
